261dac5a76a230bf1e6aaf42ebc9d15fe031de7f
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-04-27 20:27:47 -0300

    desafio arrays

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+let conteoEntradas = 0;
 console.log("//// inicio - menú principal //// \n");
 console.log("1. ingresar \n");
 console.log("2. registrarse \n");
@@ -6,9 +8,34 @@ console.log("2. registrarse \n");
 let emailUsuario;
 let passwordUsuario;
 let cupo = 200;
+let entradas = [];
+
+const usuarios = [];
+let usuario = 0;
+
 const precioEntradas = 900;
 const serviceCharge = 50;
-let entradasReservadas = 0;
+let entradasReservadas = [];
+
+class Usuario {
+	constructor(email, password) {
+		this.password = password;
+		this.email = email;
+		this.entradas = [];
+		// this.estaIngresado = false;
+	}
+	salir() {
+		document.write('saliste.');
+		// estaIngresado = false;
+	}
+}
+
+class Entrada {
+	constructor(numero, precio) {
+		this.numero = numero;
+		this.precio = precio;
+	}
+}
 
 menuPrincipal();
 
@@ -31,6 +58,17 @@ function menuPrincipal() {
 	}
 }
 
+
+function registrarse() {
+	const emailUsuario = prompt('Ingrese email');
+	const passwordUsuario = prompt('Ingrese contraseña');
+	usuario = new Usuario(emailUsuario, passwordUsuario);
+	usuarios.push(usuario);
+	menuPrincipal();
+	console.log(usuarios);
+}
+
+
 function ingresar() {
 	let email = prompt('Email: ');
 	let password = prompt('Contraseña: ');
@@ -44,22 +82,11 @@ function ingresar() {
 }
 
 function validarUsuario(email, password) {
-	if (email !== emailUsuario || password !== passwordUsuario) {
+	if (email !== usuario.email || password !== usuario.password) {
 		return false;
 	} else {
 		return true;
 	};
-}
-
-function registrarse() {
-	emailUsuario = prompt('Email: ');
-	passwordUsuario = prompt('Contraseña: ');
-	alert('el registro fue exitoso!');
-	menuPrincipal();
-}
-
-function salir() {
-	document.write('saliste.');
 }
 
 function menuMiembros() {
@@ -72,7 +99,7 @@ function menuMiembros() {
 	const seleccion2 = Number(prompt('Ingrese una opción: | 1. reservar entrada | 2. ver mi entrada | 3. ver cupos | 4. Menú principal'));
 	switch (seleccion2) {
 		case 1:
-			reservarEntrada();
+			reservarEntradas();
 			break;
 		case 2:
 			verEntradas();
@@ -91,50 +118,55 @@ function menuMiembros() {
 			menuMiembros();
 			break;
 	}
+}
 
-	function reservarEntrada() {
-		let entradasAReservar = Number(prompt('Ingrese el número de entradas a reservar'));
-		if (entradasAReservar <= cupo) {
-			calcularPrecio(entradasAReservar);
-			pagarEntradas();
-			entradasReservadas += entradasAReservar;
-			cupo -= entradasAReservar;
-			console.log('cupos: ' + cupo);
-			alert(`reservaste ${entradasAReservar} entradas.`);
-			entradasAReservar = 0;
-			menuMiembros();
-		} else {
-			alert('no quedan más entradas :(');
-			menuMiembros();
-		}
-		function calcularPrecio() {
-			let precio = 200
-			let subtotal = precio * entradasAReservar;
-			let total = subtotal + serviceCharge;
-			alert(
-				`${entradasAReservar} entradas a 200 c/u: ${subtotal}` + '\n' +
-				`Service Charge: ${serviceCharge}` + '\n' +
-				`Total: ${total}`
-			)
-			return (total);
-		}
-		function pagarEntradas(total) {
-			console.log(total);
-		}
-	}
+function reservarEntradas() {
+	// let conteoEntradas = entradasReservadas.length;
+	let cantidadEntradas = prompt('Ingrese la cantidad de entradas que desea reservar.')
 
-	function verEntradas() {
-		console.log(`Tienes ${entradasReservadas} entradas reservadas`)
-		for (let i = 0; i < entradasReservadas; i += 1) {
-			document.write('<img style={width: 300px} src=https://static4.depositphotos.com/1012407/370/v/950/depositphotos_3707681-stock-illustration-yellow-ticket.jpg>')
+	if (cantidadEntradas <= cupo) {
+		calcularPrecio(cantidadEntradas);
+		pagarEntradas();
+		for (let i = 0; i < cantidadEntradas; i += 1) {
+			// entradasReservadas.push(new Entrada(conteoEntradas += 1, 200));
+			usuario.entradas.push(new Entrada(conteoEntradas += 1, 200));
+			console.log(conteoEntradas)
+			cupo -= 1;
 		}
-		alert(`tienes ${entradasReservadas} entradas reservadas`);
-		// menuMiembros();
-	}
-
-	function verCupos() {
-		console.log(`Quedan ${cupo} entradas`);
-		alert(`Quedan ${cupo} entradas`);
+	} else {
+		alert('no quedan más entradas :(');
 		menuMiembros();
 	}
+
+	alert(`reservaste ${cantidadEntradas} entradas.`);
+	menuMiembros();
+
+	function calcularPrecio() {
+		let precio = 200
+		let subtotal = precio * cantidadEntradas;
+		let total = subtotal + serviceCharge;
+		alert(
+			`${cantidadEntradas} entradas a 200 c/u: ${subtotal}` + '\n' +
+			`Service Charge: ${serviceCharge}` + '\n' +
+			`Total: ${total}`
+		)
+		return (total);
+	}
+	function pagarEntradas(total) {
+		console.log(total);
+	}
+}
+
+function verEntradas() {
+	alert(`tienes ${usuario.entradas.length} entradas reservadas!`)
+	for (let i = 0; i < usuario.entradas.length; i += 1) {
+		console.log(`Entrada No. ${usuario.entradas[i].numero}`);
+	}
+	menuMiembros();
+}
+
+function verCupos() {
+	console.log(`Quedan ${cupo} entradas`);
+	alert(`Quedan ${cupo} entradas`);
+	menuMiembros();
 }
