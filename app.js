1c5725afaaf41d94226604f29116435a493e766e
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-05-02 19:43:22 -0300

    busqueda de entradas y username relacionado con entradas

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Pide al usuario mail y contraseña, le permite registrarse.
+// Permite reservar entradas, calcular precio, asignarle un número a cada una (mientras haya cupo).
+// Cada entrada identificada por su número está asociada al usuario.
 
-let conteoEntradas = 0;
 console.log("//// inicio - menú principal //// \n");
 console.log("1. ingresar \n");
 console.log("2. registrarse \n");
@@ -8,6 +10,7 @@ console.log("2. registrarse \n");
 let emailUsuario;
 let passwordUsuario;
 let cupo = 200;
+let conteoEntradas = 0;
 let entradas = [];
 
 const usuarios = [];
@@ -18,9 +21,10 @@ const serviceCharge = 50;
 let entradasReservadas = [];
 
 class Usuario {
-	constructor(email, password) {
-		this.password = password;
+	constructor(email, password, username) {
 		this.email = email;
+		this.password = password;
+		this.username = username
 		this.entradas = [];
 		// this.estaIngresado = false;
 	}
@@ -31,13 +35,16 @@ class Usuario {
 }
 
 class Entrada {
-	constructor(numero, precio) {
+	constructor(numero, precio, username) {
 		this.numero = numero;
 		this.precio = precio;
+		this.username = username;
 	}
 }
 
 menuPrincipal();
+
+// menuMiembros();
 
 function menuPrincipal() {
 	const selección = Number(prompt('Ingrese una opción: | 1. Ingresar | 2. registrarse | 3. salir |'));
@@ -62,7 +69,8 @@ function menuPrincipal() {
 function registrarse() {
 	const emailUsuario = prompt('Ingrese email');
 	const passwordUsuario = prompt('Ingrese contraseña');
-	usuario = new Usuario(emailUsuario, passwordUsuario);
+	const nombreUsuario = prompt('Ingrese nombre de usuario');
+	usuario = new Usuario(emailUsuario, passwordUsuario, nombreUsuario);
 	usuarios.push(usuario);
 	menuPrincipal();
 	console.log(usuarios);
@@ -94,13 +102,7 @@ function salir() {
 }
 
 function menuMiembros() {
-	console.log("1. reservar entrada \n");
-	console.log("2. ver mi entrada \n");
-	console.log("3. ver cupos \n");
-	console.log("4. menu principal");
-	// console.log("4. validar entrada \n");
-
-	const seleccion2 = Number(prompt('Ingrese una opción: | 1. reservar entrada | 2. ver mi entrada | 3. ver cupos | 4. Menú principal'));
+	const seleccion2 = Number(prompt('Ingrese una opción: | 1. reservar entrada | 2. ver mi entrada | 3. ver cupos | 4. Menú principal | 5. Buscar Entrada | 6. Cerrar Sesión'));
 	switch (seleccion2) {
 		case 1:
 			reservarEntradas();
@@ -114,14 +116,22 @@ function menuMiembros() {
 		case 4:
 			menuPrincipal();
 			break;
-		// case 4:
-		// 	validarEntrada();
-		// break;
+		case 5:
+			buscarEntradas();
+			break;
+		case 6:
+			cerrarSesion();
+			break;
 		default:
 			alert('ingrese 1 para ingresar, 2 para registrarse y 3 para salir');
 			menuMiembros();
 			break;
 	}
+}
+
+function cerrarSesion() {
+	usuario = 0;
+	menuPrincipal();
 }
 
 function reservarEntradas() {
@@ -133,15 +143,22 @@ function reservarEntradas() {
 		pagarEntradas();
 		for (let i = 0; i < cantidadEntradas; i += 1) {
 			// entradasReservadas.push(new Entrada(conteoEntradas += 1, 200));
-			usuario.entradas.push(new Entrada(conteoEntradas += 1, 200));
-			console.log(conteoEntradas)
+			let nuevaEntrada = new Entrada(conteoEntradas += 1, 200, usuario.username);
+			usuario.entradas.push(nuevaEntrada);
+			entradasReservadas.push(nuevaEntrada);
+			console.log(conteoEntradas);
+			console.log(entradasReservadas);
 			cupo -= 1;
 		}
-	} else {
+	} else if (cupo == 0) {
 		alert('no quedan más entradas :(');
 		menuMiembros();
+	} else {
+		alert(`Actualmente quedan solo ${cupo} entradas.`)
+		reservarEntradas();
 	}
 	alert(`reservaste ${cantidadEntradas} entradas.`);
+	console.log(entradas)
 	menuMiembros();
 
 	function calcularPrecio() {
@@ -173,3 +190,25 @@ function verCupos() {
 	alert(`Quedan ${cupo} entradas`);
 	menuMiembros();
 }
+
+function buscarEntradas() {
+	let numEntrada = prompt('Ingrese número de entrada a buscar');
+	let entradaEncontrada = busqueda(entradasReservadas, numEntrada);
+
+	if (entradaEncontrada != undefined) {
+		alert(`La entrada ${entradaEncontrada.numero} pertenece al usuario ${entradaEncontrada.username}`);
+		menuMiembros();
+	} else {
+		alert('La entrada no ha sido reservada.');
+	}
+
+	function busqueda(array, numEntrada) {
+		return array.find(e => e.numero == parseInt(numEntrada)); // buscar un elemento en el array cuya propiedad numero coincida con el segundo argumento de la función.
+	}
+}
+
+// entradas.push(new Entrada(5, 900, 'Gorje'));
+// entradas.push(new Entrada(9, 800, 'Pepe'));
+// menuPrincipal()
+
+
