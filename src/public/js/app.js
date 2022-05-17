11bd2fffe87233749a66a79928e008a47ce93be0
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-05-17 16:32:47 -0300

    total y array entradas

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,77 +1,73 @@
-//// ENTRADAS ////
-import { agregarEntradas, reservarEntradas } from './entradas.js';
+// Import
+import { Entrada, crearEntrada, calcularPrecio } from './entradas.js';
+import { Evento } from './eventos.js';
 import { Usuario } from './usuarios.js';
 
-const formAgregar = document.querySelector('#formAgregar');
-let inputCantidad = document.querySelector('#cantidad');
-// const botonAgregar = document.querySelector('#botonAgregar');
-const botonReservar = document.querySelector('#botonReservar');
+// Variables
+let entradasReservadas = [];
+let entradas = [];
+const eventos = [];
+const username = 'Juan01';
+let total = 0;
 
-formAgregar.addEventListener('submit', (e) => {
-	e.preventDefault();
-	let cantidad = inputCantidad.value;
-	agregarEntradas(cantidad);
-})
+let eventosGrid = document.querySelector('#eventos-grid');
+let conteo = document.querySelector('#conteo');
 
-botonReservar.addEventListener('click', (e) => {
-	e.preventDefault();
-	reservarEntradas();
-})
+// eventos.push(new Evento('La Skatomica', '6-2', 'El Club Bar', 'Honduras 5028', '6/2', '22:00', 200));
+eventos.push(new Evento(['Combustión Reggae', 'Sabancaya'], '14-3', 'Shambala House', 'Costa Rica 5673', '14-3', '22:00', 300));
+eventos.push(new Evento('La Skatomica', '20-3', 'El Club Bar', 'Honduras 5028', '6/2', '22:00', 200));
+eventos.push(new Evento('Les Pistaches', '9-4', 'Paseo Inmoral Social Club', 'Cabrera 5100', '9/04', '20:00', 500));
+eventos.push(new Evento('Perro', '16-4', 'Paseo Inmoral Social Club', 'Cabrera 5100', '16/04', '20:00', 500));
+eventos.push(new Evento(['Tchanko y Familia', 'DJ Jamming'], '17-4', 'El Club Bar', 'Honduras 5028', '17/04', '22:00', 800));
+eventos.push(new Evento(['Camaleonpaex', 'DJ Jamming'], '21-8', 'El Club Bar', 'Honduras 5028', '21-8', '22:00', 800));
+eventos.push(new Evento(['Cehache Respira', 'Ana Sol y La Candela'], '23-8', 'El Club Bar', 'Honduras 5028', '23-8', '22:00', 500));
+eventos.push(new Evento(['Sabancaya', 'DJ Jamming'], '18-9', 'El Club Bar', 'Honduras 5028', '18-9', '22:00', 800));
+eventos.push(new Evento(['Buena Cosecha', 'DJ Jamming'], '2-10', 'El Club Bar', 'Honduras 5028', '2-10', '22:00', 800));
+eventos.push(new Evento(['Bemba', 'DJ Jamming'], '16-10', 'El Club Bar', 'Honduras 5028', '16-10', '22:00', 800));
+eventos.push(new Evento(['The Walking Ska', 'Preciosa Venus'], '18-10', 'El Club Bar', 'Honduras 5028', '18-10', '22:00', 700));
+eventos.push(new Evento(['El Capitan Digital Version', 'DJ Jamming'], '30-10', 'El Club Bar', 'Honduras 5028', '30-10', '22:00', 700));
 
+for (let evento of eventos) {
+	evento.makeId();
+	// console.log(evento.getId());
 
-function menuPrincipal() {
-	const selección = Number(prompt('Ingrese una opción: | 1. Ingresar | 2. registrarse | 3. salir |'));
-	switch (selección) {
-		case 1:
-			ingresar();
-			break;
-		case 2:
-			registrarse();
-			break;
-		case 3:
-			salir();
-			break;
-		default:
-			alert('ingrese 1 para ingresar, 2 para registrarse y 3 para salir');
-			menuPrincipal();
-			break;
-	}
+	let index = eventos.indexOf(evento) + 1;
+	let divEvento = document.createElement('div');
+	divEvento.setAttribute('id', `evento-${index}`);
+	divEvento.setAttribute('class', 'div-evento');
+
+	// Agregar un form para cada evento
+	let form = document.createElement('form');
+	form.setAttribute('id', `form-${index}`)
+	form.innerHTML =
+		`
+		<label for="cantidad">Cantidad: </label>
+		<input type="number" name="input-cantidad-${index}" id="input-cantidad-${index}">
+		<button id="botonAgregar">Agregar</button>
+	`
+	divEvento.appendChild(form);
+
+	// Agregar una imagen a cada evento
+	let img = document.createElement('img');
+	img.setAttribute('src', `public/img/flyers/${evento.imagen}.jpg`);
+	divEvento.appendChild(img);
+	eventosGrid.appendChild(divEvento);
+
+	//// EVENTOS ////
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		let input = document.querySelector(`#input-cantidad-${index}`);
+		let cantidad = input.value;
+		let subtotal = calcularPrecio(cantidad, evento.precio);
+		for (let i = 0; i < cantidad; i += 1) {
+			let nuevaEntrada = crearEntrada(evento.nombre, evento.precio, username, evento.getId());
+			nuevaEntrada.makeId();
+			entradas.push(nuevaEntrada);
+			total += evento.precio;
+		}
+		console.log(`${cantidad} entradas para ${evento.nombre}......$${subtotal}`);
+		console.log(entradas);
+		conteo.innerText = `Total: ${entradas.length} entradas .... $${total}`;
+	});
+
 }
-
-function menuMiembros() {
-	const seleccion2 = Number(prompt('Ingrese una opción: | 1. reservar entrada | 2. ver mi entrada | 3. ver cupos | 4. Menú principal | 5. Buscar Entrada | 6. Cerrar Sesión'));
-	switch (seleccion2) {
-		case 1:
-			reservarEntradas();
-			break;
-		case 2:
-			verEntradas();
-			break;
-		case 3:
-			verCupos();
-			break;
-		case 4:
-			menuPrincipal();
-			break;
-		case 5:
-			buscarEntradas();
-			break;
-		case 6:
-			cerrarSesion();
-			break;
-		default:
-			alert('ingrese 1 para ingresar, 2 para registrarse y 3 para salir');
-			menuMiembros();
-			break;
-	}
-}
-
-
-function salir() {
-	document.write('saliste');
-}
-
-// entradas.push(new Entrada(5, 900, 'Gorje'));
-// entradas.push(new Entrada(9, 800, 'Pepe'));
-
-
