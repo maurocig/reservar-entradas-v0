31fb9ce7acf8e0cc0092d286fde6bcb35c55d27e
Mauro Cigliuti <mcigliuti01@gmail.com>
2022-05-31 13:16:35 -0300

    lee json

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -31,30 +31,30 @@ let usuario = new Usuario('juan@gmail.com', 'juancito', 'juan01')
 
 // import data from './data.json';
 
-fetch("./")
+fetch('/data.json')
 	.then((res) => res.json())
 	.then((data) => {
 		data.forEach((evento) => {
-			eventos.push(new Evento(evento.artista, evento.imagen, evento.lugar, evento.direccion, evento.fecha, evento.horario, evento.precio));
-			console.log(evento.nombre);
+			let nuevoEvento = new Evento(evento.artista, evento.imagen, evento.lugar, evento.direccion, evento.fecha, evento.horario, evento.precio);
+			eventos.push(nuevoEvento);
 		})
 	})
 	.catch((err) => console.log(err));
 
 
-// eventos.push(new Evento('La Skatomica', '6-2', 'El Club Bar', 'Honduras 5028', '6/2', '22:00', 200));
-eventos.push(new Evento(['Combustión Reggae', 'Sabancaya'], '14-3', 'Shambala House', 'Costa Rica 5673', '14-3', '22:00', 300));
-eventos.push(new Evento('La Skatomica', '20-3', 'El Club Bar', 'Honduras 5028', '6/2', '22:00', 200));
-eventos.push(new Evento('Les Pistaches', '9-4', 'Paseo Inmoral Social Club', 'Cabrera 5100', '9-4', '20:00', 500));
-eventos.push(new Evento('Perro', '16-4', 'Paseo Inmoral Social Club', 'Cabrera 5100', '16-04', '20:00', 500));
-eventos.push(new Evento(['Tchanko y Familia', 'DJ Jamming'], '17-4', 'El Club Bar', 'Honduras 5028', '17/04', '22:00', 800));
-eventos.push(new Evento(['Camaleonpaex', 'DJ Jamming'], '21-8', 'El Club Bar', 'Honduras 5028', '21-8', '22:00', 800));
-eventos.push(new Evento(['Cehache Respira', 'Ana Sol y La Candela'], '23-8', 'El Club Bar', 'Honduras 5028', '23-8', '22:00', 500));
-eventos.push(new Evento(['Sabancaya', 'DJ Jamming'], '18-9', 'El Club Bar', 'Honduras 5028', '18-9', '22:00', 800));
-eventos.push(new Evento(['Buena Cosecha', 'DJ Jamming'], '2-10', 'El Club Bar', 'Honduras 5028', '2-10', '22:00', 800));
-eventos.push(new Evento(['Bemba', 'DJ Jamming'], '16-10', 'El Club Bar', 'Honduras 5028', '16-10', '22:00', 800));
-eventos.push(new Evento(['The Walking Ska', 'Preciosa Venus'], '18-10', 'El Club Bar', 'Honduras 5028', '18-10', '22:00', 700));
-eventos.push(new Evento(['El Capitan Digital Version', 'DJ Jamming'], '30-10', 'El Club Bar', 'Honduras 5028', '30-10', '22:00', 700));
+// // eventos.push(new Evento('La Skatomica', '6-2', 'El Club Bar', 'Honduras 5028', '6/2', '22:00', 200));
+// eventos.push(new Evento(['Combustión Reggae', 'Sabancaya'], '14-3', 'Shambala House', 'Costa Rica 5673', '14-3', '22:00', 300));
+// eventos.push(new Evento('La Skatomica', '20-3', 'El Club Bar', 'Honduras 5028', '6/2', '22:00', 200));
+// eventos.push(new Evento('Les Pistaches', '9-4', 'Paseo Inmoral Social Club', 'Cabrera 5100', '9-4', '20:00', 500));
+// eventos.push(new Evento('Perro', '16-4', 'Paseo Inmoral Social Club', 'Cabrera 5100', '16-04', '20:00', 500));
+// eventos.push(new Evento(['Tchanko y Familia', 'DJ Jamming'], '17-4', 'El Club Bar', 'Honduras 5028', '17/04', '22:00', 800));
+// eventos.push(new Evento(['Camaleonpaex', 'DJ Jamming'], '21-8', 'El Club Bar', 'Honduras 5028', '21-8', '22:00', 800));
+// eventos.push(new Evento(['Cehache Respira', 'Ana Sol y La Candela'], '23-8', 'El Club Bar', 'Honduras 5028', '23-8', '22:00', 500));
+// eventos.push(new Evento(['Sabancaya', 'DJ Jamming'], '18-9', 'El Club Bar', 'Honduras 5028', '18-9', '22:00', 800));
+// eventos.push(new Evento(['Buena Cosecha', 'DJ Jamming'], '2-10', 'El Club Bar', 'Honduras 5028', '2-10', '22:00', 800));
+// eventos.push(new Evento(['Bemba', 'DJ Jamming'], '16-10', 'El Club Bar', 'Honduras 5028', '16-10', '22:00', 800));
+// eventos.push(new Evento(['The Walking Ska', 'Preciosa Venus'], '18-10', 'El Club Bar', 'Honduras 5028', '18-10', '22:00', 700));
+// eventos.push(new Evento(['El Capitan Digital Version', 'DJ Jamming'], '30-10', 'El Club Bar', 'Honduras 5028', '30-10', '22:00', 700));
 
 for (let evento of eventos) {
 	evento.makeId();
